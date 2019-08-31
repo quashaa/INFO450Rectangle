312b7d1cfaed961afebcba96be3aa9c7fa863522
Andrew Quash <quashaa@vcu.edu>
2019-08-31 13:38:54 -0400

    Add project files.

---
+++ Rectangle/Source.cpp
@@ -0,0 +1,26 @@
+//program to calculate the area & perimeter of a rectangle
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float length;
+	float width;
+	float perimeter;
+	float area;
+
+	cout << "Please enter the length of the retangle below:" << endl;
+	cin >> length;
+
+	cout << "\nPlease enter the width of the rectangle below:" << endl;
+	cin >> width;
+
+	area = length * width;
+	perimeter = 2 * (length + width);
+
+	cout << "\nThe area of the rectangle is " << area << " units" << endl;
+	cout << "The perimeter of the rectangle is " << perimeter << " units\n" << endl;
+	system("pause");
+
+	return 0;
+}
